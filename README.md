fed91bf500516e7e01e049d4f50f86d017fb2870
Christophe <christophevelou@gmail.com>
2021-02-21 15:38:11 +0100

    first commit

--- README.md
+++ README.md
@@ -22,3 +22,42 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+********************************************************************************************
+
+0/Pré-requis nodeJS d'installé:
+npm install -g @vue/cli | vue create <name-project> | cd <name-project> | npm run serve
+
+npm run serve:
+
+1/Formulaire de base avc les inputs : lié nos données à ds input, on utilise 'v-model'
+*Pas d'action en attribut dans la balise <form> car on ne fait que la partie "front"...
+*class form group (form-control dans les inputs egalement) pr styliser le tout via bootstrap
+*Il faut lié nos data() à nos input via v-model !
+*Interpolation , composant moustache sous les Résultats
+
+2/Astuce formulaire :
+* Mettre toutes ls données relative à un formulaire via formData: {}
+*Dans le <textarea>, "white-space: pre" : pr passer à la ligne en appuyant sur entrée
+
+3/ Gérer les checkbox :
+Très important de renseigner "value", puis de faire v-model et le lier à un [] dans le <form> et l'afficher avc une boucle for pour l'afficher en dehors du <form>
+v-bind:key="index" pour fruit | index unique à chaque éléménet de liste que l'on va créer
+
+4/Gerer les select Box : pas besoin d'ID , Vue ns propose qqch, on fait juste une balise <option>
+et on complete le formdData (via select:'' et listePays: ['xxx','xxx'...]) de data()
+v-model="formData.select" pour le <select>, le lié à nos data()!
+
+
+5/Envoyer les données (submit): comment envoyer les données en local.
+On a pas de backend, mais par exemple avec firebase
+un formulaire ça marche soit avec un "input" type submit ou un <button>...
+* v-if="infoSubmit" : div que l'on affiche seulement apres avoir valider les données
+* v-on:click.prevent="envoiForm" et on crée la fct 'envoiForm' dans methods, le prevent pour maintenir le result et donc l'affichage apres avoir submit le result
+* v-on: input (un évènement)
+
+
+
+
+
+
---
+++ src/components/Contenu.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container mt-5">
+        <h1>Notre premier formulaire </h1>
+        <form>
+            <div class="form-group">
+                <label for="prenom">Ton prénom</label>
+                <input v-on:input="toggleResult" v-model="formData.prenom" type="text" id="prenom" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="txt">Ton texte</label>
+                <textarea v-on:input="toggleResult" v-model="formData.txt" class="form-control" id="txt"></textarea>
+            </div>
+
+            <h3>Select Box</h3>
+
+            <select v-on:input="toggleResult" v-model="formData.select">
+                <option v-bind:key="index" v-for="(pays, index) in formData.listePays">{{ pays }}</option>
+            </select>
+
+            <h3 class="mt-3">CheckBoxs</h3>
+
+            <div class="form-check">
+                <input v-on:input="toggleResult" v-model="formData.checkFruits" value="fraise" id="fraise" type="checkbox" class="form-check-input">
+                <label for="fraise">Fraise</label>
+            </div>
+
+            <div class="form-check">
+                <input v-on:input="toggleResult" v-model="formData.checkFruits" value="pomme" id="pomme" type="checkbox" class="form-check-input">
+                <label for="pomme">Pomme</label>
+            </div>
+
+            <div class="form-check">
+                <input v-on:input="toggleResult" v-model="formData.checkFruits"  value="cerise" id="cerise" type="checkbox" class="form-check-input">
+                <label for="cerise">Cerise</label>
+            </div>
+
+            <button v-on:click.prevent="envoiForm" class="btn btn-primary mt-3 mb-3">Envoyez les données</button>
+        </form>
+        <div v-if="infoSubmit">
+            <h2>Résultats</h2>
+                    <div class="card p-3">
+                        <p>Prénom : {{ formData.prenom }} </p>
+                        <p style="white-space: pre">Texte : {{ formData.txt }} </p>
+
+                        <p>Résultats checkbox</p>
+                        <ul>
+                            <li v-bind:key="index" v-for="(fruit, index) in formData.checkFruits"> {{ fruit }}</li>
+                        </ul>
+                        <p>Choix du select : {{ formData.select }}</p>
+                    </div>
+        </div>
+    </div>
+</template>
+
+<script>    
+    export default {
+        name: 'Contenu',
+        data(){
+            return {
+                formData: { //pr mieux organiser les données
+                    prenom: '',
+                    txt: '',
+                    checkFruits: [],
+                    select: '',
+                    listePays: ['Russie', 'Japon', 'Suede', 'France', 'Angleterre']
+                },
+                infoSubmit: false // on le cache au depart
+            }
+        },
+        methods: {
+           envoiForm: function(){
+               this.infoSubmit = true;
+           },
+           toggleResult: function(){
+               this.infoSubmit = false;
+           }
+        }
+    }
+</script>
+
+<style>
+    h1 {
+        margin-top: 100px!important;
+    }
+    .onglets{
+        height: 300px;
+    }
+</style>
